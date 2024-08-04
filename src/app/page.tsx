98a86ec693d93ba4556064082a1bb42a5f429f54
Mayank Tilwankar <mayanktilwankar2355@gmail.com>
2024-08-04 15:47:48 +0530

    pitme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
 import { Boxes } from "@/components/ui/background-boxes";
 import { cn } from "@/lib/utils";
 import { FlipWords } from "@/components/ui/flip-words";
-import {TextRevealCard} from "@/components/ui/text-reveal-card";
+import { TextRevealCard } from "@/components/ui/text-reveal-card";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 import Services from "@/components/Services";
 import Campaigns from "@/components/Campaigns";
-import Clients from "@/components/Clients"
+import Clients from "@/components/Clients";
 import Image from "next/image";
+import Footer from "@/components/Footer";
 
 const navItems = [
   {
@@ -25,117 +26,116 @@ const navItems = [
   {
     name: "Career",
     link: "/career",
-    icon: (
-      <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-    ),
+    icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
   },
   {
     name: "Campaigns",
     link: "#campaigns",
-    icon: (
-      <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-    ),
+    icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
   },
   {
     name: "Our Services",
     link: "#services",
-    icon: (
-      <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-    ),
+    icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
   },
   {
     name: "Our Clients",
     link: "#clients",
-    icon: (
-      <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-    ),
-  }
+    icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
 ];
 
-const words = ["Content-Creation", "Meme-Marketing", "Social-Media-Management", "Graphic-Designing", "Organic-Content", "Web-Development"];
+const words = [
+  "Content-Creation",
+  "Meme-Marketing",
+  "Social-Media-Management",
+  "Graphic-Designing",
+  "Organic-Content",
+  "Web-Development",
+];
 
 export default function Home() {
   return (
     <>
-
-
- <div className="relative  w-full">
-      <FloatingNav navItems={navItems} />
-    </div>
-
-
-    <div id="home">
-    <HeroHighlight>
-    <Image src={"https://i.ibb.co/R4653Hf/PITCHHME-FULL-LOGO.png" } alt={"blaH"} width={800} height={800} quality={100}/>
-    </HeroHighlight>
-    </div>
-    
-    <div id="about">
-    <HeroHighlight >
-    <div className="absolute z-50 inset-0 flex items-start -my-52 justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          Who are we?
-        </p>
+      <div className="relative  w-full">
+        <FloatingNav navItems={navItems} />
       </div>
-    <div className="text-6xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        We are a Content Marketing Firm specializing in
-        <br />
-        <div className="flex justify-center pt-10">
-        <FlipWords words={words} />  
-        </div>
-        
+
+      <div id="home">
+        <HeroHighlight>
+          <Image
+            src={"https://i.ibb.co/R4653Hf/PITCHHME-FULL-LOGO.png"}
+            alt={"blaH"}
+            width={800}
+            height={800}
+            quality={100}
+          />
+        </HeroHighlight>
       </div>
-    </HeroHighlight>
-    </div>
 
-
-    <div id="services">
-    <HeroHighlight>
-    <div className="absolute z-50 inset-0 flex items-start -my-16 justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          What do we do?
-        </p>
+      <div id="about">
+        <HeroHighlight>
+          <div className="absolute z-50 inset-0 flex items-start -my-52 justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+              Who are we?
+            </p>
+          </div>
+          <div className="text-6xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+            We are a Content Marketing Firm specializing in
+            <br />
+            <div className="flex justify-center pt-10">
+              <FlipWords words={words} />
+            </div>
+          </div>
+        </HeroHighlight>
       </div>
-      <div>
-      <Services />
 
+      <div id="services">
+        <HeroHighlight>
+          <div className="absolute z-50 inset-0 flex items-start -my-16 justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+              What do we do?
+            </p>
+          </div>
+          <div>
+            <Services />
+          </div>
+        </HeroHighlight>
       </div>
-    </HeroHighlight>
-    </div>
-    
-<div id="campaigns">
-<HeroHighlight>
-<div className="absolute z-50 inset-0 flex items-start -my-48 justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          ... and these are our meme campaigns! 
-        </p>
+
+      <div id="campaigns">
+        <HeroHighlight>
+          <div className="absolute z-50 inset-0 flex items-start -my-48 justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+              ... and these are our meme campaigns!
+            </p>
+          </div>
+          <div>
+            <Campaigns />
+          </div>
+        </HeroHighlight>
       </div>
-      <div>
-      <Campaigns />
 
+      <div id="clients">
+        <HeroHighlight>
+          <div className="absolute z-50 inset-0 flex items-start -my-52 justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+              Look who we worked for!
+            </p>
+          </div>
+          <div>
+            <Clients />
+          </div>
+        </HeroHighlight>
       </div>
-    </HeroHighlight>
-</div>
-    
 
-    <div id="clients">
-    <HeroHighlight>
-    <div className="absolute z-50 inset-0 flex items-start -my-52 justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          Look who we worked for!
-        </p>
+      <div id="enquiry">
+        <HeroHighlight>
+          <div className="flex justify-center items-end">
+          <Footer />
+          </div>
+        </HeroHighlight>
       </div>
-      <div>
-      <Clients />
-
-      </div>
-    </HeroHighlight>
-
-    </div>
-
-  
-    
-    
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,142 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { ChangeEvent } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { HeroHighlight } from "@/components/ui/hero-highlight";
+
+function SignupFormDemo() {
+  const router = useRouter();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("firstName", firstName);
+    formData.append("lastName", lastName);
+    formData.append("email", email);
+
+    const response = await fetch(
+      "https://ok7-ts-backend.adaptable.app/api/save-details",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    router.push("/");
+  };
+
+  return (
+    <div
+      style={{
+        background:
+          "linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0))",
+        backdropFilter: "blur(2px)",
+        WebkitBackdropFilter: "blur(2px)",
+        border: "1px solid rgba(255, 255, 255, 0.18)",
+        boxShadow: "0 8px 32px 0 rgba(0, 0, 0, 0.37)",
+      }}
+      className="max-w-md scale-90 w-full mt-16 mx-auto rounded-none md:rounded-xl p-4 md:p-8 shadow-input bg-white dark:bg-transparent border"
+    >
+      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+        Welcome to Pitchhme
+      </h2>
+      <form className="my-8" onSubmit={handleSubmit}>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer>
+            <Label htmlFor="firstname">First name</Label>
+            <Input
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+              id="firstname"
+              placeholder="John"
+              type="text"
+            />
+          </LabelInputContainer>
+          <LabelInputContainer>
+            <Label htmlFor="lastname">Last name</Label>
+            <Input
+              id="lastname"
+              placeholder="Doe"
+              type="text"
+              value={lastName}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+            />
+          </LabelInputContainer>
+        </div>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email Address</Label>
+          <Input
+            id="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="john.dev@gmail.com"
+            type="email"
+          />
+        </LabelInputContainer>
+
+        <button
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          Send Details &rarr;
+          <BottomGradient />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
+
+const foot = () => {
+  return (
+    <>
+    <div className="flex-row ">
+      <div>
+        <SignupFormDemo />
+      </div>
+      <div className="flex items-end">
+      <button className="inline-flex h-12 my-20 w-screen animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        Shimmer
+      </button>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default foot;
