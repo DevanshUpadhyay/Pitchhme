f67a6662b0db3ad737fce6ee977e2253175fc001
unknown <aaradhya.korde@gmail.com>
2024-08-16 22:21:19 +0530

    meowmeow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <Script>
+        <Script id="hi">
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
